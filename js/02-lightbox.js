2c2d6de8d263cc8f92aa5f98d4459966c2b84283
dmitkate <44602492+dmitkate@users.noreply.github.com>
2022-08-22 21:00:09 +0300

    fixaion

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+
+const galleryItem = galleryItems.map((item) =>
+    `<a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+        </a>`)
+    .join("");
+
+gallery.innerHTML = galleryItem;
+
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    overlayOpacity: 1,
+    captionsData: "alt",
+    captionDelay: 250,
+    });
